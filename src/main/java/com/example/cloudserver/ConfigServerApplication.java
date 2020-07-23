3b794f7897ff3a319d00ba3c092b23f73c0383de
Mário Calegaro <mcalegaro@gmail.com>
2020-07-22 22:42:51 -0300

    protect '/decrypt'

--- src/main/java/com/example/cloudserver/ConfigServerApplication.java
+++ src/main/java/com/example/cloudserver/ConfigServerApplication.java
@@ -2,14 +2,33 @@ package com.example.cloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+
+				.csrf().disable()
+
+				.authorizeRequests()
+
+				.antMatchers("/decrypt").authenticated()
+
+				.and()
+
+				.authorizeRequests().antMatchers("/**").permitAll();
+
 	}
 
 }
